8952b3bbacd8ce0963633ee56d8fa5e3cfda95ce
Błażej Pietrzyk <bpietrzyk@ALFAVOX.LOCAL>
2017-06-06 13:42:05 +0200

    Working integration send email test

--- forget_about_emails/apps/mail_sender/config/test.exs
+++ forget_about_emails/apps/mail_sender/config/test.exs
@@ -2,11 +2,11 @@ use Mix.Config
 
 config :my_app, MailSender.Mailer,
   adapter: Bamboo.SMTPAdapter,
-  server: "smtp.domain",
-  port: 1025,
-  username: "your.name@your.domain", # or {:system, "SMTP_USERNAME"}
-  password: "pa55word", # or {:system, "SMTP_PASSWORD"}
+  server: "smtp.gmail.com",
+  port: 465,
+  username: "blazej.pietrzyk@gmail.com", # or {:system, "SMTP_USERNAME"}
+  password: "", # or {:system, "SMTP_PASSWORD"}
   tls: :if_available, # can be `:always` or `:never`
   allowed_tls_versions: [:"tlsv1", :"tlsv1.1", :"tlsv1.2"], # or {":system", ALLOWED_TLS_VERSIONS"} w/ comma seprated values (e.g. "tlsv1.1,tlsv1.2")
-  ssl: false, # can be `true`
+  ssl: true, # can be `true`
   retries: 1
--- forget_about_emails/apps/mail_sender/test/mail_sender_test.exs
+++ forget_about_emails/apps/mail_sender/test/mail_sender_test.exs
@@ -2,4 +2,8 @@ defmodule MailSenderTest do
   use ExUnit.Case
   doctest MailSender.MailFormatter
 
+  test "send email" do
+    mail_def = %MailDef{:to => "b.pietrzyk@outlook.com", :from => "blazej.pietrzyk@gmail.com", :subject => "email subject", :text => "email text", :html => "email html"}
+    MailSender.Mailer.send_now(mail_def)
+  end
 end
